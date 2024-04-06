55d803fe5e8f4ae2defc824bba10c480c4d7b24e
Adnan Karim <adnan.karim@ucalgary.ca>
2024-04-06 17:35:13 -0600

    tiptap ingtegration is annoying. more front end changes

---
+++ FlipFlopWeb/src/Components/HeaderStartup.tsx
@@ -0,0 +1,12 @@
+import flipFlopLogo from '../assets/flipFlopTransparent.png'
+import { Link } from 'react-router-dom'; 
+function HeaderStartup ()
+{
+    return(
+        <Link to="/">
+            <img src={flipFlopLogo} alt="Image" style={{ maxWidth: '10%', maxHeight: '10%', objectFit: 'contain', paddingLeft:20 }} />
+        </Link>
+    )
+}
+
+export default HeaderStartup;
